d70ff53c35722c4e569fdb1eee77c70fbf1dd029
Maksym Martynenko <predatar24@gmail.com>
2023-05-25 14:19:48 +0300

    add backend

---
+++ backend/src/api/hot-offer/controllers/hot-offer.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hot-offer controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hot-offer.hot-offer');
