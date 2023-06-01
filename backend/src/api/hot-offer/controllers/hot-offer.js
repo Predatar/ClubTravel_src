8487ef1c5cef697857164d9e72f55cf1f07443a3
Maksym Martynenko <predatar24@gmail.com>
2023-06-01 12:15:27 +0300

    delete backend

--- backend/src/api/hot-offer/controllers/hot-offer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * hot-offer controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::hot-offer.hot-offer');
